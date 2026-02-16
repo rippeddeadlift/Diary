d9306223c100df100e2a1c2352bf7fa1a8954f44
Rip <ivan.kurilin@gmail.com>
2026-02-16 21:53:18 +0100

    ref

--- diary-app/src/data/tagConfig.ts
+++ diary-app/src/data/tagConfig.ts
@@ -9,4 +9,4 @@ export const PEOPLE = [
   'mksaf'
 ] as const
 
-export const TAGS = ['family', 'nature', 'cycling', 'art'] as const
+export const TAGS = ['family', 'nature', 'cycling', 'art', 'travel', 'attractions'] as const
