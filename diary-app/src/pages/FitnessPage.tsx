ccac1ef968397a0692324ffd0decc9e0e7535eb4
Rip <ivan.kurilin@gmail.com>
2026-02-20 01:01:37 +0100

    ref

--- diary-app/src/pages/FitnessPage.tsx
+++ diary-app/src/pages/FitnessPage.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { CounterTracker } from '@/components/fitness/CounterTracker'
