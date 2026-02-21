e21b2b0fa6769d6c3dc30bb603eb7849a1c8a591
Rip <ivan.kurilin@gmail.com>
2026-02-21 02:28:08 +0100

    updated trip data

--- diary-app/src/data/tagConfig.ts
+++ diary-app/src/data/tagConfig.ts
@@ -14,5 +14,5 @@ export type Person = (typeof PEOPLE)[number]
 export const PHOTO_TAGS = ['family', 'friends', 'food', 'nature', 'cycling', 'art', 'travel', 'sights'] as const
 export type PhotoTag = (typeof PHOTO_TAGS)[number]
 
-export const TRIP_ACTIVITY_TAGS = ['cycling', 'running', 'hiking', 'skiing', 'kayaking'] as const
+export const TRIP_ACTIVITY_TAGS = ['cycling', 'running', 'hiking', 'skiing', 'kayaking', 'skating'] as const
 export type TripActivityTag = (typeof TRIP_ACTIVITY_TAGS)[number]
